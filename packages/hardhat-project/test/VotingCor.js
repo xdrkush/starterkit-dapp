9e50bde49396ff68251220026c6277c622aeba11
morgan.brisson <morgan.brisson@aquilapp.com>
2023-06-05 13:35:03 +0200

    fix: add comment & modulation fixture

--- packages/hardhat-project/test/VotingCor.js
+++ packages/hardhat-project/test/VotingCor.js
@@ -14,36 +14,27 @@ describe("VotingCor", function () {
         const votingCor = await VotingCor.deploy();
         await votingCor.deployed();
 
-        const [owner, addr1, addr2] = await ethers.getSigners();
+        // Get provider with signer
+        const [owner, addr1, addr2, addr3] = await ethers.getSigners();
 
-        return { votingCor, owner, addr1, addr2 };
+        return { votingCor, owner, addr1, addr2, addr3 };
     };
 
     async function deployFixtureProposal() {
-        // Déploiement du contrat
-        const VotingCor = await ethers.getContractFactory("VotingCor");
-        const votingCor = await VotingCor.deploy();
-        await votingCor.deployed();
+        // Call child fixture 
+        const { votingCor, owner, addr1, addr2, addr3 } = await loadFixture(deployFixtureRegisterVoter)
 
-        const [owner, addr1, addr2] = await ethers.getSigners();
-
-        await votingCor.addVoter(addr1.address);
-
-        return { votingCor, owner, addr1, addr2 };
-    };
-
-    async function deployFixtureVote() {
-        // Déploiement du contrat
-        const VotingCor = await ethers.getContractFactory("VotingCor");
-        const votingCor = await VotingCor.deploy();
-        await votingCor.deployed();
-
-        const [owner, addr1, addr2, addr3] = await ethers.getSigners();
-
-        // Create 2 Voters
+        // Add Voters (2)
         await votingCor.addVoter(addr1.address);
         await votingCor.addVoter(addr2.address);
 
+        return { votingCor, owner, addr1, addr2, addr3 };
+    };
+
+    async function deployFixtureVote() {
+        const { votingCor, owner, addr1, addr2, addr3 } = await loadFixture(deployFixtureProposal)
+
+        // Start registering Vote
         await votingCor.startProposalsRegistering()
 
         // Create 3 Proposals
@@ -51,31 +42,16 @@ describe("VotingCor", function () {
         await votingCor.connect(addr2).addProposal("Proposal 2")
         await votingCor.connect(addr1).addProposal("Proposal 3")
 
+        // Close Registering Proposal
         await votingCor.endProposalsRegistering()
 
         return { votingCor, owner, addr1, addr2, addr3 };
     };
 
     async function deployFixtureTallied() {
-        // Déploiement du contrat
-        const VotingCor = await ethers.getContractFactory("VotingCor");
-        const votingCor = await VotingCor.deploy();
-        await votingCor.deployed();
+        const { votingCor, owner, addr1, addr2, addr3 } = await loadFixture(deployFixtureVote)
 
-        const [owner, addr1, addr2, addr3] = await ethers.getSigners();
-
-        // Create 2 Voters
-        await votingCor.addVoter(addr1.address);
-        await votingCor.addVoter(addr2.address);
-
-        await votingCor.startProposalsRegistering()
-
-        // Create 3 Proposals
-        await votingCor.connect(addr1).addProposal("Proposal 1")
-        await votingCor.connect(addr2).addProposal("Proposal 2")
-        await votingCor.connect(addr1).addProposal("Proposal 3")
-
-        await votingCor.endProposalsRegistering()
+        // Start voting session
         await votingCor.startVotingSession()
 
         // 2 vote on proposal 2
@@ -91,41 +67,47 @@ describe("VotingCor", function () {
 
             it("should have correct owner", async () => {
                 const { votingCor, owner } = await loadFixture(deployFixtureRegisterVoter)
+                // Check is good owner
                 expect(await votingCor.owner()).to.equal(owner.address);
             });
 
-            it("should have correct registration", async () => {
+            it("should have correct registration voter", async () => {
                 const { votingCor, addr1 } = await loadFixture(deployFixtureRegisterVoter)
                 const addr1Providder = votingCor.connect(addr1)
 
+                // Add new voter
                 await votingCor.addVoter(addr1.address);
 
+                // Check if voter is register
                 expect((await addr1Providder.getVoter(addr1.address))[0]).to.equal(true);
             });
 
-            it("should have correct addr2 is not registration", async () => {
+            it("should have correct addr2 is not registration to voter", async () => {
                 const { votingCor, addr1, addr2 } = await loadFixture(deployFixtureRegisterVoter)
                 const addr1Providder = votingCor.connect(addr1)
 
+                // Add new voter
                 await votingCor.addVoter(addr1.address);
 
+                // Check if voter is not register
                 expect((await addr1Providder.getVoter(addr2.address))[0]).to.equal(false);
             });
 
             it("should have correct error if is already register", async () => {
                 const { votingCor, addr1 } = await loadFixture(deployFixtureRegisterVoter)
 
+                // Add new voter
                 await votingCor.addVoter(addr1.address);
 
-                // Not voter
+                // Check if voter is already register
                 await expect(votingCor.addVoter(addr1.address))
                     .to.be.revertedWith("Already registered")
             });
 
-            it("should have correct not getVoter if not Voter", async () => {
+            it("should have correct not getVoter if not voter", async () => {
                 const { votingCor, addr1 } = await loadFixture(deployFixtureRegisterVoter)
 
-                // Not voter
+                // Check if not a voter
                 await expect(votingCor.getVoter(addr1.address))
                     .to.be.revertedWith("You're not a voter")
             });
@@ -138,7 +120,7 @@ describe("VotingCor", function () {
                 const { votingCor, addr1 } = await loadFixture(deployFixtureProposal)
                 const addr1Provider = votingCor.connect(addr1)
 
-                // Check
+                // Check if already voter
                 expect((await addr1Provider.getVoter(addr1.address))[0]).to.equal(true);
 
                 // Status for Create Proposal
@@ -147,16 +129,17 @@ describe("VotingCor", function () {
                 // Get Proposal
                 const proposal = await addr1Provider.getOneProposal(0)
 
+                // Check if good proposal
                 expect(proposal[0]).to.equal("GENESIS"); // description
                 expect(proposal[1]).to.equal(0); // voteCount
 
             });
 
-            it("should have correct add new proposal", async () => {
+            it("should have correct for add new proposal", async () => {
                 const { votingCor, addr1 } = await loadFixture(deployFixtureProposal)
                 const addr1Provider = votingCor.connect(addr1)
 
-                // Check
+                // Check if already voter
                 expect((await addr1Provider.getVoter(addr1.address))[0]).to.equal(true);
 
                 // Status for Create Proposal
@@ -168,16 +151,17 @@ describe("VotingCor", function () {
                 // Get Proposal
                 const proposal = await addr1Provider.getOneProposal(1)
 
+                // Check new proposal
                 expect(proposal[0]).to.equal("Proposal 1"); // description
                 expect(proposal[1]).to.equal(0); // voteCount
 
             });
 
-            it("should have correct dont create new proposal", async () => {
+            it("should have correct status for dont create new proposal", async () => {
                 const { votingCor, addr1 } = await loadFixture(deployFixtureProposal)
                 const addr1Provider = votingCor.connect(addr1)
 
-                // New status : Create Proposal -> Err
+                // Check status not ready for register proposal
                 await expect(addr1Provider.addProposal("Proposal 1"))
                     .to.be.revertedWith("Proposals are not allowed yet")
 
@@ -190,7 +174,7 @@ describe("VotingCor", function () {
                 // Status for Create Proposal
                 await votingCor.startProposalsRegistering()
 
-                // New status : Create Proposal -> Err
+                // Check if no propose empty proposal
                 await expect(addr1Provider.addProposal(""))
                     .to.be.revertedWith("Vous ne pouvez pas ne rien proposer")
 
@@ -200,6 +184,7 @@ describe("VotingCor", function () {
                 const { votingCor, addr1 } = await loadFixture(deployFixtureProposal)
                 const addr1Provider = votingCor.connect(addr1)
 
+                // Stop status registering proposal
                 votingCor.endProposalsRegistering()
 
                 // New status : Create Proposal -> Err
@@ -211,16 +196,18 @@ describe("VotingCor", function () {
 
         describe("Vote", () => {
 
-            it("should have correct status is note open vote registration", async () => {
+            it("should have correct status is not open vote registration", async () => {
                 const { votingCor, addr1, addr2 } = await loadFixture(deployFixtureVote)
                 const addr1Provider = votingCor.connect(addr1)
 
-                // Check
+                // Check voters
                 expect((await addr1Provider.getVoter(addr1.address))[0]).to.equal(true);
                 expect((await addr1Provider.getVoter(addr2.address))[0]).to.equal(true);
+                // Check Proposal
                 expect((await addr1Provider.getOneProposal(0))[0]).to.equal('GENESIS');
                 expect((await addr1Provider.getOneProposal(3))[0]).to.equal('Proposal 3');
 
+                // Check if not status for vote
                 await expect(addr1Provider.setVote(1))
                     .to.be.revertedWith("Voting session havent started yet")
 
@@ -240,46 +227,19 @@ describe("VotingCor", function () {
                 // Start session voting
                 await votingCor.startVotingSession()
 
-                // // Create Proposal
+                // Create Proposal (addr1)
                 await addr1Provider.setVote(1)
-                // // Get Proposal
+                // Check Get Proposal
                 expect((await addr1Provider.getOneProposal(1))[1]).to.equal(1);
 
-                // // Create Proposal
+                // Create Proposal (addr2)
                 await addr2Provider.setVote(1)
-                // // Get Proposal
+                // Check Get Proposal
                 expect((await addr2Provider.getOneProposal(1))[1]).to.equal(2);
 
 
             });
             
-            it("should have correct vote registration", async () => {
-                const { votingCor, addr1, addr2 } = await loadFixture(deployFixtureVote)
-                const addr1Provider = votingCor.connect(addr1)
-                const addr2Provider = votingCor.connect(addr2)
-
-                // Check
-                expect((await addr1Provider.getVoter(addr1.address))[0]).to.equal(true);
-                expect((await addr1Provider.getVoter(addr2.address))[0]).to.equal(true);
-                expect((await addr1Provider.getOneProposal(0))[0]).to.equal('GENESIS');
-                expect((await addr1Provider.getOneProposal(3))[0]).to.equal('Proposal 3');
-
-                // Start session voting
-                await votingCor.startVotingSession()
-
-                // // Create Proposal
-                await addr1Provider.setVote(1)
-                // // Get Proposal
-                expect((await addr1Provider.getOneProposal(1))[1]).to.equal(1);
-
-                // // Create Proposal
-                await addr2Provider.setVote(1)
-                // // Get Proposal
-                expect((await addr2Provider.getOneProposal(1))[1]).to.equal(2);
-
-
-            });
-
             it("should have correct error for vote by no Voters", async () => {
                 const { votingCor, addr3 } = await loadFixture(deployFixtureVote)
                 const addr3Provider = votingCor.connect(addr3)
@@ -287,6 +247,7 @@ describe("VotingCor", function () {
                 // Start session voting
                 await votingCor.startVotingSession()
 
+                // Check if voter no register can't vote
                 await expect(addr3Provider.setVote(1))
                     .to.be.revertedWith("You're not a voter")
 
@@ -296,15 +257,14 @@ describe("VotingCor", function () {
 
         describe("Tallied", () => {
 
-            it("should have noy correct status for tallied", async () => {
+            it("should have correct status for dont have tallied", async () => {
                 const { votingCor, addr1 } = await loadFixture(deployFixtureTallied)
                 const addr1Provider = votingCor.connect(addr1)
 
-                // Check
+                // Check get proposal
                 expect((await addr1Provider.getOneProposal(2))[1]).to.equal(2);
 
-                votingCor.tallyVotes()
-
+                // Check if not good status for tallyVotes
                 await expect(votingCor.tallyVotes())
                     .to.be.revertedWith("Current status is not voting session ended")
             });
@@ -313,72 +273,57 @@ describe("VotingCor", function () {
                 const { votingCor, addr1 } = await loadFixture(deployFixtureTallied)
                 const addr1Provider = votingCor.connect(addr1)
 
-                // Check
+                // Check proposal
                 expect((await addr1Provider.getOneProposal(2))[1]).to.equal(2);
                 
+                // Stop session vote
                 await votingCor.endVotingSession()
                 
+                // Tally vote
                 votingCor.tallyVotes()
 
+                // Check if a good winning proposal
                 expect(Number(await votingCor.winningProposalID())).to.equal(2) // getWinningProposal
 
             });
 
-            // it("should have correct proposal winning public", async () => {
-            //     const { voting, addr1, addr3 } = await loadFixture(deployFixtureTallied)
-
-            //     expect(await voting.isWhitelisted(addr1.address)).to.equal(true);
-
-            //     voting.incWorkflowStatus() // Stop Session Vote
-            //     voting.incWorkflowStatus() // Vote Tallied
-            //     expect(await voting.getStatus()).to.equal(5);
-
-            //     const proposalWin = await voting.connect(addr3).getWinningProposal()
-
-            //     expect(proposalWin[0]).to.equal("Proposal 2") // getWinningProposal
-
-            // });
-
-            // it("should have correct status end proposal", async () => {
-            //     const { voting } = await loadFixture(deployFixtureTallied)
-            //     voting.votingSessionEnded() // Stop Session Vote
-            //     expect(await voting.getStatus()).to.equal(4);
-            //     voting.incWorkflowStatus() // Vote Tallied
-            //     expect(await voting.getStatus()).to.equal(5);
-
-            // });
-
         })
 
         describe("Event", () => {
-            it("VoterRegistered:event", async () => {
+            it("Event -> VoterRegistered", async () => {
                 const { votingCor, addr1 } = await loadFixture(deployFixtureRegisterVoter)
 
+                // Check if event return good value
                 await expect(votingCor.addVoter(addr1.address))
                     .to.emit(votingCor, "VoterRegistered")
                     .withArgs(String);
             })
-            it("WorkflowStatusChange:event", async () => {
+            it("Event -> WorkflowStatusChange", async () => {
                 const { votingCor } = await loadFixture(deployFixtureRegisterVoter)
 
+                // Check if event return good value
                 await expect(votingCor.startProposalsRegistering())
                     .to.emit(votingCor, "WorkflowStatusChange")
                     .withArgs(0, 1);
             })
-            it("ProposalRegistered:event", async () => {
+            it("Event -> ProposalRegistered", async () => {
                 const { votingCor, addr1 } = await loadFixture(deployFixtureProposal)
 
+                // Start registering proposal
                 await votingCor.startProposalsRegistering()
 
+                // Check if event return good value
                 await expect(votingCor.connect(addr1).addProposal("Proposal 1"))
                     .to.emit(votingCor, "ProposalRegistered")
                     .withArgs(1);
             })
-            it("Voted:event", async () => {
+            it("Event -> Voted", async () => {
                 const { votingCor, addr1 } = await loadFixture(deployFixtureVote)
 
+                // Start voting session
                 await votingCor.startVotingSession()
 
+                // Check if event return good value
                 await expect(votingCor.connect(addr1).setVote(1))
                     .to.emit(votingCor, "Voted")
                     .withArgs(String, String);
