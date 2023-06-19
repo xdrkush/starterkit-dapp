c2ef12eab84506959983975f4223904c66cbe002
xdrkush <drkushikush@gmail.com>
2023-06-19 21:08:45 +0200

    fix: DOS_GAS_LIMIT in contract Voting.sol with test

--- packages/hardhat-project/hardhat.config.js
+++ packages/hardhat-project/hardhat.config.js
@@ -7,6 +7,9 @@ const { INFURA_API_KEY, SEPOLIA_PRIVATE_KEY, GOERLI_PRIVATE_KEY } = process.env
 module.exports = {
   solidity: "0.8.19",
   networks: {
+    hardhat: {
+      blockGasLimit: 3000000 // Default 30_000_000
+    },
     // sepolia: {
     //   url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
     //   accounts: [SEPOLIA_PRIVATE_KEY]
