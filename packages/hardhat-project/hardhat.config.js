c3771c956c4698feb767550c03131ee714191e75
xdrkush <drkushikush@gmail.com>
2023-06-19 15:10:11 +0200

    fix: deploy on goerli

--- packages/hardhat-project/hardhat.config.js
+++ packages/hardhat-project/hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+const { INFURA_API_KEY, SEPOLIA_PRIVATE_KEY, GOERLI_PRIVATE_KEY } = process.env
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    // sepolia: {
+    //   url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+    //   accounts: [SEPOLIA_PRIVATE_KEY]
+    // },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  }
 };
